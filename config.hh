67751e04ee768f6a4c4b689d1f8cd7c7cbdd44db
Wladimir J. van der Laan <laanwj@gmail.com>
2014-07-15 11:26:24 +0200

    Update SAMPLE_MULT_OUTPUT_FLOAT to 0.33

--- config.hh
+++ config.hh
@@ -15,7 +15,7 @@ static const unsigned MaxSamplesAtTime = 512; // 512=dbopl limitation
 static const unsigned MaxWidth = 160;
 static const unsigned MaxHeight = 1 + 23*MaxCards;
 static const unsigned SAMPLE_MULT_FACTOR = 10240; // Conversion from float to 16-bit signed sample
-static const float SAMPLE_MULT_OUTPUT_FLOAT = 0.25f; // Scaling applied to output samples
+static const float SAMPLE_MULT_OUTPUT_FLOAT = 0.33f; // Scaling applied to output samples
 static const double ReverbScale = 0.1;
 
 extern unsigned AdlBank;
