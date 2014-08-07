493566f2c7a74adbf034434ba16f65677011cc75
Wladimir J. van der Laan <laanwj@gmail.com>
2014-08-07 22:00:42 +0200

    Wrap AlsaSeqListener in a class

--- seqplay.cc
+++ seqplay.cc
@@ -29,11 +29,6 @@
 #include "ui.hh"
 
 static volatile sig_atomic_t QuitFlag = false;
-static volatile bool terminateAlsaThread = false;
-static snd_seq_t *seq;
-static int port_count;
-static snd_midi_event_t *midi_enc;
-
 const uint64_t NANOS_PER_S = 1000000000LL;
 
 // Add this number of samples to time for new midi events to make sure that
@@ -45,57 +40,6 @@ static void TidyupAndExit(int)
     UI.Cleanup();
     signal(SIGINT, SIG_DFL);
     raise(SIGINT);
-    terminateAlsaThread = true;
-}
-
-/* prints an error message to stderr, and dies */
-static void fatal(const char *msg, ...)
-{
-    va_list ap;
-
-    va_start(ap, msg);
-    vfprintf(stderr, msg, ap);
-    va_end(ap);
-    fputc('\n', stderr);
-    exit(EXIT_FAILURE);
-}
-
-/* error handling for ALSA functions */
-static void check_snd(const char *operation, int err)
-{
-    if (err < 0)
-        fatal("Cannot %s - %s", operation, snd_strerror(err));
-}
-
-static void init_seq()
-{
-    int err;
-
-    /* open sequencer */
-    err = snd_seq_open(&seq, "default", SND_SEQ_OPEN_DUPLEX, 0);
-    check_snd("open sequencer", err);
-
-    /* set our client's name */
-    err = snd_seq_set_client_name(seq, "adlmidi");
-    check_snd("set client name", err);
-
-    /* create event encoder */
-    err = snd_midi_event_new(16, &midi_enc);
-    check_snd("create midi event encoder", err);
-    /* always write command bytes */
-    snd_midi_event_no_status(midi_enc, 1);
-}
-
-static void create_port(void)
-{
-    int err;
-
-    err = snd_seq_create_simple_port(seq, "adlmidi",
-                     SND_SEQ_PORT_CAP_WRITE |
-                     SND_SEQ_PORT_CAP_SUBS_WRITE,
-                     SND_SEQ_PORT_TYPE_MIDI_GENERIC |
-                     SND_SEQ_PORT_TYPE_APPLICATION);
-    check_snd("create port", err);
 }
 
 class MidiEventQueue
@@ -250,8 +194,83 @@ void Clock::Sync(uint32_t out_samples)
     }
 }
 
-Clock *midiclock;
-MidiEventQueue *midiqueue;
+/* Listen to ALSA Midi events */
+class AlsaSeqListener
+{
+public:
+    AlsaSeqListener(Clock *midiclock, MidiEventQueue *midiqueue);
+    ~AlsaSeqListener();
+
+    /* Start listening to ALSA events */
+    void Start();
+    /* Stop listening to ALSA events */
+    void Stop();
+private:
+    Clock *midiclock;
+    MidiEventQueue *midiqueue;
+    SDL_Thread *alsa_thread;
+    volatile bool terminate;
+    snd_seq_t *seq;
+    int port_count;
+    snd_midi_event_t *midi_enc;
+
+    void Run();
+    void init_seq();
+    void create_port();
+    void handle_alsa_event(uint32_t timestamp, const snd_seq_event_t *ev);
+
+    friend int AlsaThread(void*);
+};
+
+/* prints an error message to stderr, and dies */
+static void fatal(const char *msg, ...)
+{
+    va_list ap;
+
+    va_start(ap, msg);
+    vfprintf(stderr, msg, ap);
+    va_end(ap);
+    fputc('\n', stderr);
+    exit(EXIT_FAILURE);
+}
+
+/* error handling for ALSA functions */
+static void check_snd(const char *operation, int err)
+{
+    if (err < 0)
+        fatal("Cannot %s - %s", operation, snd_strerror(err));
+}
+
+void AlsaSeqListener::init_seq()
+{
+    int err;
+
+    /* open sequencer */
+    err = snd_seq_open(&seq, "default", SND_SEQ_OPEN_DUPLEX, 0);
+    check_snd("open sequencer", err);
+
+    /* set our client's name */
+    err = snd_seq_set_client_name(seq, "adlmidi");
+    check_snd("set client name", err);
+
+    /* create event encoder */
+    err = snd_midi_event_new(16, &midi_enc);
+    check_snd("create midi event encoder", err);
+    /* always write command bytes */
+    snd_midi_event_no_status(midi_enc, 1);
+}
+
+void AlsaSeqListener::create_port()
+{
+    int err;
+
+    err = snd_seq_create_simple_port(seq, "adlmidi",
+                     SND_SEQ_PORT_CAP_WRITE |
+                     SND_SEQ_PORT_CAP_SUBS_WRITE,
+                     SND_SEQ_PORT_TYPE_MIDI_GENERIC |
+                     SND_SEQ_PORT_TYPE_APPLICATION);
+    check_snd("create port", err);
+}
 
 /**
  * in second thread, add incoming MIDI events to queue with timestamp (in nanos)
@@ -268,7 +287,7 @@ MidiEventQueue *midiqueue;
  * Define midi event type with timestamp in samples and either embedded data or pointer to sysex (or ignore sysex for now).
  * Use a std::deque as event queue, or a ring buffer, protected by mutex.
  */
-static void handle_alsa_event(MidiEventQueue *evh, uint32_t timestamp, const snd_seq_event_t *ev)
+void AlsaSeqListener::handle_alsa_event(uint32_t timestamp, const snd_seq_event_t *ev)
 {
     // Handle SysEx as special case as it is already in raw format
     if(ev->type == SND_SEQ_EVENT_SYSEX)
@@ -276,7 +295,7 @@ static void handle_alsa_event(MidiEventQueue *evh, uint32_t timestamp, const snd
         if(ev->data.ext.len >= 1)
         {
             const unsigned char *data = (unsigned char*)ev->data.ext.ptr;
-            evh->PushEvent(timestamp, 0, data[0], &data[1], ev->data.ext.len-1);
+            midiqueue->PushEvent(timestamp, 0, data[0], &data[1], ev->data.ext.len-1);
         }
         return;
     }
@@ -290,17 +309,68 @@ static void handle_alsa_event(MidiEventQueue *evh, uint32_t timestamp, const snd
     {
         unsigned char byte = buf[ptr++];
         unsigned length = MidiEventLength(byte);
-        evh->PushEvent(timestamp, 0, byte, &buf[ptr], length);
+        midiqueue->PushEvent(timestamp, 0, byte, &buf[ptr], length);
         ptr += length;
     }
 }
 
-int AlsaThread(void *)
+int AlsaThread(void *listener)
+{
+    static_cast<AlsaSeqListener*>(listener)->Run();
+    return 0;
+}
+
+AlsaSeqListener::AlsaSeqListener(Clock *midiclock, MidiEventQueue *midiqueue):
+    midiclock(midiclock),
+    midiqueue(midiqueue),
+    alsa_thread(0),
+    terminate(false),
+    seq(0),
+    port_count(0),
+    midi_enc(0)
+{
+}
+
+AlsaSeqListener::~AlsaSeqListener()
+{
+    Stop();
+}
+
+void AlsaSeqListener::Start()
+{
+    int err;
+    /// XXX need a way to enumerate ports and choose one
+    init_seq();
+    create_port();
+    err = snd_seq_nonblock(seq, 1);
+    check_snd("set nonblock mode", err);
+    if (port_count > 0)
+        UI.InitMessage(-1, "Waiting for data.");
+    else
+        UI.InitMessage(-1, "Waiting for data at port %d:0.",
+               snd_seq_client_id(seq));
+    UI.InitMessage(-1, "\n");
+
+    alsa_thread = SDL_CreateThread(AlsaThread, this);
+    terminate = false;
+}
+
+void AlsaSeqListener::Stop()
+{
+    if(seq == 0)
+        return;
+    terminate = true;
+    SDL_WaitThread(alsa_thread, NULL);
+    alsa_thread = 0;
+    snd_seq_close(seq);
+}
+
+void AlsaSeqListener::Run()
 {
     int err;
     int npfds = snd_seq_poll_descriptors_count(seq, POLLIN);
     struct pollfd *pfds = (struct pollfd *)alloca(sizeof(*pfds) * npfds);
-    while(!terminateAlsaThread)
+    while(!terminate)
     {
         snd_seq_poll_descriptors(seq, pfds, npfds, POLLIN);
         int rv = poll(pfds, npfds, 100);
@@ -312,57 +382,18 @@ int AlsaThread(void *)
             if (err < 0)
                 break;
             if (event)
-                handle_alsa_event(midiqueue, midiclock->CurrentSamples() + MIDI_DELAY_FRAMES, event);
+                handle_alsa_event(midiclock->CurrentSamples() + MIDI_DELAY_FRAMES, event);
         } while (err > 0);
     }
-    return 0;
 }
 
-int main(int argc, char** argv)
+
+/* Synthesis loop: read events from queue and write audio */
+void SynthLoop(Clock *midiclock, MidiEventQueue *midiqueue)
 {
-    // How long is SDL buffer, in seconds?
-    // The smaller the value, the more often SDL_AudioCallBack()
-    // is called.
-    const double AudioBufferLength = 0.025;
-    // How much do WE buffer, in seconds? The smaller the value, // the more prone to sound chopping we are.
-    const double OurHeadRoomLength = 0.05;
-    // The lag between visual content and audio content equals
-    // the sum of these two buffers.
-
-    UI.InitMessage(15, "ADLSEQ: OPL3 softsynth for Linux\n");
-    UI.InitMessage(3, "(C) -- https://github.com/laanwj/adlmidi\n");
-
-    signal(SIGTERM, TidyupAndExit);
-    signal(SIGINT, TidyupAndExit);
-
-    InitializeAudio(AudioBufferLength, OurHeadRoomLength);
-    int rv = ParseArguments(argc, argv);
-    if(rv >= 0)
-        return rv;
-
-    // ALSA
-    int err;
-    init_seq();
-    create_port();
-    err = snd_seq_nonblock(seq, 1);
-    check_snd("set nonblock mode", err);
-    if (port_count > 0)
-        UI.InitMessage(-1, "Waiting for data.");
-    else
-        UI.InitMessage(-1, "Waiting for data at port %d:0.",
-               snd_seq_client_id(seq));
-    UI.InitMessage(-1, "\n");
-    SDL_Thread *thread = SDL_CreateThread(AlsaThread, 0);
-
-    StartAudio();
-
-    UI.StartGrid();
-
     MIDIeventhandler evh;
     evh.Reset();
 
-    midiclock = new Clock((AudioBufferLength + OurHeadRoomLength) * NANOS_PER_S);
-    midiqueue = new MidiEventQueue();
     uint32_t cur_samples = 0;
     while( !QuitFlag )
     {
@@ -395,15 +426,53 @@ int main(int argc, char** argv)
         }
         midiclock->Sync(cur_samples);
     }
+}
+
+int main(int argc, char** argv)
+{
+    // How long is SDL buffer, in seconds?
+    // The smaller the value, the more often SDL_AudioCallBack()
+    // is called.
+    const double AudioBufferLength = 0.025;
+    // How much do WE buffer, in seconds? The smaller the value, // the more prone to sound chopping we are.
+    const double OurHeadRoomLength = 0.05;
+    // The lag between visual content and audio content equals
+    // the sum of these two buffers.
+
+    UI.InitMessage(15, "ADLSEQ: OPL3 softsynth for Linux\n");
+    UI.InitMessage(3, "(C) -- https://github.com/laanwj/adlmidi\n");
+
+    signal(SIGTERM, TidyupAndExit);
+    signal(SIGINT, TidyupAndExit);
+
+    InitializeAudio(AudioBufferLength, OurHeadRoomLength);
+    int rv = ParseArguments(argc, argv);
+    if(rv >= 0)
+        return rv;
+
+    Clock *midiclock;
+    MidiEventQueue *midiqueue;
+    midiclock = new Clock((AudioBufferLength + OurHeadRoomLength) * NANOS_PER_S);
+    midiqueue = new MidiEventQueue();
+
+    AlsaSeqListener *seqin = new AlsaSeqListener(midiclock, midiqueue);
+    seqin->Start();
+
+    StartAudio();
+
+    UI.StartGrid();
+
+    SynthLoop(midiclock, midiqueue);
 
     ShutdownAudio();
 
-    // Terminate ALSA
-    terminateAlsaThread = true;
-    SDL_WaitThread(thread, NULL);
+    delete seqin;
+    seqin = 0;
     delete midiclock;
+    midiclock = 0;
     delete midiqueue;
-    snd_seq_close(seq);
+    midiqueue = 0;
+
     UI.Cleanup();
     return 0;
 }
